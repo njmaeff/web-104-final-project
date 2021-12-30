a2a000976e87a4e0d661d4e1604be51a33a7cb52
Nik Jmaeff <nik@jmaeff.me>
2021-12-30 11:53:45 -0800

    make code more consistent

--- src/above-and-beyond/pages/api/export/pdfTemplate.ts
+++ src/above-and-beyond/pages/api/export/pdfTemplate.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-
 export const makeRateReport = async (token, params) => {
+
+    // get html for report
     const html = await axios.get('/rate/export', {
         params,
         headers: {
@@ -10,11 +11,14 @@ export const makeRateReport = async (token, params) => {
         baseURL: process.env.NEXT_PUBLIC_WEB_HOST
     })
 
-    const pdf = await axios.post(`${process.env.NEXT_PUBLIC_WEB_HOST}/api/pdf`, {html: html.data}, {
+
+    // generate pdf from html
+    const pdf = await axios.post('/api/pdf', {html: html.data}, {
         headers: {
             authorization: 'Bearer ' + token
         },
-        responseType: "arraybuffer"
+        responseType: "arraybuffer",
+        baseURL: process.env.NEXT_PUBLIC_WEB_HOST
     });
     return pdf.data;
 };
